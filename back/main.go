a45dcac08829c6006d8f2d7ff769845be19d47ad
rdzPedraos <rdzpedraos@gmail.com>
2025-04-13 21:57:43 -0500

    refactor: set main.go out of the src, and delete folder

--- back/main.go
+++ back/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"app/config"
+	"app/db"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	router := gin.Default()
+	db.DBConnection()
+	cfg := config.Get()
 
+	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
-	router.Run(":3000")
+	router.Run(fmt.Sprintf("%s:%s", cfg.Server.Host, cfg.Server.Port))
 }

982ab414d4870f684fe04bdc6ca4bde7315c6e64
rdzPedraos <rdzpedraos@gmail.com>
2025-04-18 18:07:28 -0500

    feat: add prettier

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,13 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
-})
+    modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image', '@nuxt/test-utils', '@nuxt/ui'],
+});

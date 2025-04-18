6cb6174b0c741973cf2ed840d7ed93298b3a1de1
rdzPedraos <rdzpedraos@gmail.com>
2025-04-18 18:33:26 -0500

    feat: first version for layout

---
+++ client/layouts/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <div>
+        <header class="flex items-center gap-4 border-b-2 border-gray-100 bg-white px-3 py-4">
+            <AppLogo />
+
+            <nav class="flex items-center gap-4">
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </nav>
+        </header>
+
+        <main>
+            <slot></slot>
+        </main>
+    </div>
+</template>

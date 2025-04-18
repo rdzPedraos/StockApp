bff21d4fe249d6ef30d1342dba2873ecc52ee04c
rdzPedraos <rdzpedraos@gmail.com>
2025-04-18 18:33:38 -0500

    fix: avoid validate close tags because has conflict with prettier

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt();
-// Your custom configs here
+export default withNuxt({
+    rules: {
+        'vue/html-self-closing': [
+            'error',
+            {
+                html: {
+                    void: 'any', // Permitir cualquier estilo para elementos vacíos HTML
+                    normal: 'never',
+                    component: 'always',
+                },
+                svg: 'always',
+                math: 'always',
+            },
+        ],
+    },
+});

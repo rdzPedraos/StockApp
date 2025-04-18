53ca421b8c192d38a125715a7b329dff3256d35f
rdzPedraos <rdzpedraos@gmail.com>
2025-04-18 18:33:14 -0500

    feat: add app logo component

---
+++ client/components/AppLogo.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex h-8 items-center gap-1">
+        <img src="/img/logo.png" alt="logo" class="h-full object-contain" />
+        <span class="text-2xl font-bold">StockApp</span>
+    </div>
+</template>

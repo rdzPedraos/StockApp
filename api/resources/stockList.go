7eda9cdd71679473f6f2f54ed82a75fce39f0d26
rdzPedraos <rdzpedraos@gmail.com>
2025-04-22 18:31:52 -0500

    fix: change structure in stock list resource

--- api/resources/stockList.go
+++ api/resources/stockList.go
@@ -8,18 +8,14 @@ import (
 	"time"
 )
 
-type stockRecommendation struct {
-	Time           time.Time `json:"time"`
-	Recommendation string    `json:"recommendation"`
-}
-
 type stockListItem struct {
-	Ticker             string              `json:"ticker"`
-	Company            string              `json:"company"`
-	Price              float64             `json:"price"`
-	MarketCap          float64             `json:"market_cap"`
-	LastRecommendation stockRecommendation `json:"last_recommendation"`
-	Logo               string              `json:"logo"`
+	Ticker     string    `json:"ticker"`
+	Company    string    `json:"company"`
+	Price      float64   `json:"price"`
+	MarketCap  float64   `json:"market_cap"`
+	Logo       string    `json:"logo"`
+	LastReview time.Time `json:"last_review"`
+	Sentiment  string    `json:"sentiment"`
 }
 
 // StockListResource implementa la interfaz TypedResource para formatear listas de stocks
@@ -27,13 +23,14 @@ type StockListResource struct{}
 
 // Format formatea un ticker específico
 func (s StockListResource) Format(ticker models.Ticker) interface{} {
-	// Preparar la recomendación
-	lastRecommendation := stockRecommendation{}
+	var (
+		lastReview time.Time
+		sentiment  string
+	)
+
 	if len(ticker.Recommendations) > 0 {
-		lastRecommendation = stockRecommendation{
-			Time:           ticker.Recommendations[0].Time,
-			Recommendation: ticker.Recommendations[0].RatingTo.Label(),
-		}
+		lastReview = ticker.Recommendations[0].Time
+		sentiment = ticker.Recommendations[0].RatingTo.Label()
 	}
 
 	price := make(chan float64, 1)
@@ -43,12 +40,13 @@ func (s StockListResource) Format(ticker models.Ticker) interface{} {
 	go s.GetMarketCap(ticker, marketCap)
 
 	return stockListItem{
-		Ticker:             ticker.ID,
-		Company:            ticker.Company,
-		Price:              <-price,
-		MarketCap:          <-marketCap,
-		LastRecommendation: lastRecommendation,
-		Logo:               config.Server.Url + "/api/tickers/" + ticker.ID + "/logo",
+		Ticker:     ticker.ID,
+		Company:    ticker.Company,
+		Price:      <-price,
+		MarketCap:  <-marketCap,
+		LastReview: lastReview,
+		Sentiment:  sentiment,
+		Logo:       config.Server.Url + "/api/tickers/" + ticker.ID + "/logo",
 	}
 }
 
